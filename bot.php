ceeb3f1608d11ef7c00a12a9647dda77bcef9f02
watcharajeewi <35356635+watcharajeewi@users.noreply.github.com>
2018-05-10 13:49:04 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -33,9 +33,11 @@ if (!is_null($events['events'])) {
 				'text' => $text
 				];*/
 			$messages = [
-				"type"=> "image",
-				    "originalContentUrl"=> "https://www.telegraph.co.uk/content/dam/Travel/2017/April/view-stonehange.jpg",
-				    "previewImageUrl"=> "https://www.telegraph.co.uk/content/dam/Travel/2017/April/view-stonehange.jpg"
+				"type": "location",
+				    "title": "my location",
+				    "address": "〒150-0002 東京都渋谷区渋谷２丁目２１−１",
+				    "latitude": 35.65910807942215,
+				    "longitude": 139.70372892916203
 				];
 			
 
