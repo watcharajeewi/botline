bc8e9e2d59f45726bcbf1f64860d58f434b3e500
watcharajeewi <35356635+watcharajeewi@users.noreply.github.com>
2018-05-10 13:38:49 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -29,9 +29,9 @@ if (!is_null($events['events'])) {
 			
 			// Build message to reply back
 			$messages = [
-				"type": "text",
-				    "text": "\uDBC0\uDC84 LINE emoji"
-			];
+				'type' => 'text',
+				'text' => $text
+				];
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
