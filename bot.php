2a925e50cb9888e2339d947bcbc06bfc3169872d
watcharajeewi <35356635+watcharajeewi@users.noreply.github.com>
2018-05-10 14:54:39 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -40,7 +40,7 @@ if (!is_null($events['events'])) {
 			*/
 			$messages = [
 				  "type"=>"imagemap",
-				  "baseUrl"=>'https://www.telegraph.co.uk/content/dam/Travel/2017/April/view-stonehange.jpg',
+				  "baseUrl"=>'https://www.car.tabienrod.com/ses.jpg',
 				  "altText"=>"This is an imagemap",
 				  "baseSize"=>array(
 					  "height"=> 1040,
