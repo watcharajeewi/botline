c39550a87386e2fe43ed366d4fccd8af6fb7635d
watcharajeewi <35356635+watcharajeewi@users.noreply.github.com>
2019-08-15 15:20:59 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -12,8 +12,8 @@ foreach ($events['events'] as $event) {
 // Reply only when message sent is in 'text' format
 if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 // Get text sent
-$text = $event['source']['userId'];
-//  $text = $event['message']['text'];
+//$text = $event['source']['userId'];
+  $text = $event['message']['text'];
 // Get replyToken
 $replyToken = $event['replyToken'];
 // Build message to reply back
