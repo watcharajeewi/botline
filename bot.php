a8e42f5bad24db8ff605bbba00d391a5fa417b41
watcharajeewi <35356635+watcharajeewi@users.noreply.github.com>
2019-09-12 17:50:27 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -20,129 +20,31 @@ if (!is_null($events['events'])) {
 			
 			
 			if($text=="งานประมูลที่กำลังจะถึง"){
-				$messages = ['{
-  "type": "flex",
-  "altText": "Flex Message",
-  "contents": {
-    "type": "bubble",
-    "header": {
-      "type": "box",
-      "layout": "horizontal",
-      "contents": [
-        {
-          "type": "text",
-          "text": "งานประมูลที่กำลังจะถึง",
-          "size": "sm",
-          "weight": "bold",
-          "color": "#AAAAAA"
-        }
-      ]
-    },
-    "body": {
-      "type": "box",
-      "layout": "horizontal",
-      "spacing": "md",
-      "contents": [
-        {
-          "type": "box",
-          "layout": "vertical",
-          "flex": 1,
-          "contents": [
-            {
-              "type": "image",
-              "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/02_1_news_thumbnail_1.png",
-              "gravity": "bottom",
-              "size": "sm",
-              "aspectRatio": "4:3",
-              "aspectMode": "cover"
-            },
-            {
-              "type": "image",
-              "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/02_1_news_thumbnail_2.png",
-              "margin": "md",
-              "size": "sm",
-              "aspectRatio": "4:3",
-              "aspectMode": "cover"
-            },
-            {
-              "type": "image",
-              "url": "https://developers.line.me/assets/images/services/bot-designer-icon.png"
-            }
-          ]
-        },
-        {
-          "type": "box",
-          "layout": "vertical",
-          "flex": 2,
-          "contents": [
-            {
-              "type": "text",
-              "text": "7 Things to Know for Today",
-              "flex": 1,
-              "size": "xs",
-              "gravity": "top"
-            },
-            {
-              "type": "separator"
-            },
-            {
-              "type": "text",
-              "text": "Hay fever goes wild",
-              "flex": 2,
-              "size": "xs",
-              "gravity": "center"
-            },
-            {
-              "type": "separator"
-            },
-            {
-              "type": "text",
-              "text": "LINE Pay Begins Barcode Payment Service",
-              "flex": 2,
-              "size": "xs",
-              "gravity": "center"
-            },
-            {
-              "type": "separator"
-            },
-            {
-              "type": "text",
-              "text": "LINE Adds LINE Wallet",
-              "flex": 1,
-              "size": "xs",
-              "gravity": "bottom"
-            }
-          ]
-        }
-      ]
-    }
-  }
-}'];
+				$messages='';
 			
-				$data = [
-				'replyToken' => $replyToken,
-				'messages' => [$messages],
-				];
-				$post = json_encode($data);
 			}else{
-				$text='';	
+				$text='รอการตรวจสอบ';	
 				$messages = [
 				'type' => 'text',
 				'text' => $text
 				];
 			
-				$data = [
-				'replyToken' => $replyToken,
-				'messages' => [$messages],
-				];
-				$post = json_encode($data);
+				
 			}
 			
-			
-			
-			$url = 'https://api.line.me/v2/bot/message/reply';
+				$text = 'xx';
+				$messages = [
+					'type' => 'text',
+					'text' => $text
+				];
 				
-					
+				$url = 'https://api.line.me/v2/bot/message/reply';
+				$data = [
+					'replyToken' => $replyToken,
+					'messages' => [$messages],
+				];
+				
+				$post = json_encode($data);
 		
 			
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
