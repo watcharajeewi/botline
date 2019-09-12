4ad4be7ec3609dfadee15a40c57d1206e2389e4f
watcharajeewi <35356635+watcharajeewi@users.noreply.github.com>
2019-09-13 00:58:50 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -21,101 +21,121 @@ if (!is_null($events['events'])) {
 			
 			if($text=="งานประมูลที่กำลังจะถึง"){
 				$jsonFlex = [
-   "type": "flex",
-  "altText": "Flex Message",
-  "contents": [
-    "type": "bubble",
-    "header": [
-      "type": "box",
-      "layout": "horizontal",
-      "contents": [
-        [
-          "type": "text",
-          "text": "งานประมูลที่กำลังจะถึง",
-          "size": "sm",
-          "weight": "bold",
-          "color": "#AAAAAA"
-        ]
-      ]
-    ],
-    "body": [
-      "type": "box",
-      "layout": "horizontal",
-      "spacing": "md",
-      "contents": [
-        [
-          "type": "box",
-          "layout": "vertical",
-          "flex": 1,
-          "contents": [
-            [
-              "type": "image",
-              "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/02_1_news_thumbnail_1.png",
-              "gravity": "bottom",
-              "size": "sm",
-              "aspectRatio": "4:3",
-              "aspectMode": "cover"
-            ],
-            [
-              "type": "image",
-              "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/02_1_news_thumbnail_2.png",
-              "margin": "md",
-              "size": "sm",
-              "aspectRatio": "4:3",
-              "aspectMode": "cover"
-            ],
-            [
-              "type": "image",
-              "url": "https://developers.line.me/assets/images/services/bot-designer-icon.png"
-            ]
-          ]
-        ],
-        [
-          "type": "box",
-          "layout": "vertical",
-          "flex": 2,
-          "contents": [
-            [
-              "type": "text",
-              "text": "7 Things to Know for Today",
-              "flex": 1,
-              "size": "xs",
-              "gravity": "top"
-            ],
-            [
-              "type": "separator"
-            ],
-            [
-              "type": "text",
-              "text": "Hay fever goes wild",
-              "flex": 2,
-              "size": "xs",
-              "gravity": "center"
-            ],
-            [
-              "type": "separator"
-            ],
-            [
-              "type": "text",
-              "text": "LINE Pay Begins Barcode Payment Service",
-              "flex": 2,
-              "size": "xs",
-              "gravity": "center"
-            ],
-            [
-              "type": "separator"
-            ],
-            [
-              "type": "text",
-              "text": "LINE Adds LINE Wallet",
-              "flex": 1,
-              "size": "xs",
-              "gravity": "bottom"
-            ]
+  "type" => "flex",
+    "altText" => "Hello Flex Message",
+    "contents" => [
+      "type" => "bubble",
+      "direction" => "ltr",
+      "header" => [
+        "type" => "box",
+        "layout" => "vertical",
+        "contents" => [
+          [
+            "type" => "text",
+            "text" => "Purchase",
+            "size" => "lg",
+            "align" => "start",
+            "weight" => "bold",
+            "color" => "#009813"
+          ],
+          [
+            "type" => "text",
+            "text" => "฿ 100.00",
+            "size" => "3xl",
+            "weight" => "bold",
+            "color" => "#000000"
+          ],
+          [
+            "type" => "text",
+            "text" => "Rabbit Line Pay",
+            "size" => "lg",
+            "weight" => "bold",
+            "color" => "#000000"
+          ],
+          [
+            "type" => "text",
+            "text" => "2019.02.14 21:47 (GMT+0700)",
+            "size" => "xs",
+            "color" => "#B2B2B2"
+          ],
+          [
+            "type" => "text",
+            "text" => "Payment complete.",
+            "margin" => "lg",
+            "size" => "lg",
+            "color" => "#000000"
           ]
         ]
+      ],
+      "body" => [
+        "type" => "box",
+        "layout" => "vertical",
+        "contents" => [
+          [
+            "type" => "separator",
+            "color" => "#C3C3C3"
+          ],
+          [
+            "type" => "box",
+            "layout" => "baseline",
+            "margin" => "lg",
+            "contents" => [
+              [
+                "type" => "text",
+                "text" => "Merchant",
+                "align" => "start",
+                "color" => "#C3C3C3"
+              ],
+              [
+                "type" => "text",
+                "text" => "BTS 01",
+                "align" => "end",
+                "color" => "#000000"
+              ]
+            ]
+          ],
+          [
+            "type" => "box",
+            "layout" => "baseline",
+            "margin" => "lg",
+            "contents" => [
+              [
+                "type" => "text",
+                "text" => "New balance",
+                "color" => "#C3C3C3"
+              ],
+              [
+                "type" => "text",
+                "text" => "฿ 45.57",
+                "align" => "end"
+              ]
+            ]
+          ],
+          [
+            "type" => "separator",
+            "margin" => "lg",
+            "color" => "#C3C3C3"
+          ]
+        ]
+      ],
+      "footer" => [
+        "type" => "box",
+        "layout" => "horizontal",
+        "contents" => [
+          [
+            "type" => "text",
+            "text" => "View Details",
+            "size" => "lg",
+            "align" => "start",
+            "color" => "#0084B6",
+            "action" => [
+              "type" => "uri",
+              "label" => "View Details",
+              "uri" => "https://google.co.th/"
+            ]
+          ]
+        ]
       ]
-    ]
     ]
   ];
 			
