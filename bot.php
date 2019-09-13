25f3e7f07b4baf7721d29461689eb6d16dc73965
watcharajeewi <35356635+watcharajeewi@users.noreply.github.com>
2019-09-13 18:12:13 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -21,125 +21,35 @@ if (!is_null($events['events'])) {
 			
 			if($text=="งานประมูลที่กำลังจะถึง"){
 			$jsonFlex = [
-  "type" => "flex",
-    "altText" => "งานประมูลที่กำลังจะถึง",
-    "contents" => [
-      "type" => "bubble",
-      "direction" => "ltr",
-      "header" => [
-        "type" => "box",
-        "layout" => "vertical",
-        "contents" => [
-          [
-            "type" => "text",
-            "text" => "งานประมูลที่กำลังจะถึง",
-            "size" => "lg",
-            "align" => "start",
-            "weight" => "bold",
-            "color" => "#009813"
-          ],
-	  
-          [
-            "type" => "text",
-            "text" => "฿ 100.00",
-            "size" => "3xl",
-            "weight" => "bold",
-            "color" => "#000000"
-          ],
-          [
-            "type" => "text",
-            "text" => "Rabbit Line Pay",
-            "size" => "lg",
-            "weight" => "bold",
-            "color" => "#000000"
-          ],
-          [
-            "type" => "text",
-            "text" => "2019.02.14 21:47 (GMT+0700)",
-            "size" => "xs",
-            "color" => "#B2B2B2"
-          ],
-          [
-            "type" => "text",
-            "text" => "Payment complete.",
-            "margin" => "lg",
-            "size" => "lg",
-            "color" => "#000000"
-          ]
-        ]
-      ],
-      "body" => [
-        "type" => "box",
-        "layout" => "vertical",
-        "contents" => [
-          [
-            "type" => "separator",
-            "color" => "#C3C3C3"
-          ],
-          [
-            "type" => "box",
-            "layout" => "baseline",
-            "margin" => "lg",
-            "contents" => [
-              [
-                "type" => "text",
-                "text" => "Merchant",
-                "align" => "start",
-                "color" => "#C3C3C3"
-              ],
-              [
-                "type" => "text",
-                "text" => "BTS 01",
-                "align" => "end",
-                "color" => "#000000"
-              ]
-            ]
-          ],
-          [
-            "type" => "box",
-            "layout" => "baseline",
-            "margin" => "lg",
-            "contents" => [
-              [
-                "type" => "text",
-                "text" => "New balance",
-                "color" => "#C3C3C3"
-              ],
-              [
-                "type" => "text",
-                "text" => "฿ 45.57",
-                "align" => "end"
-              ]
-            ]
-          ],
-          [
-            "type" => "separator",
-            "margin" => "lg",
-            "color" => "#C3C3C3"
-          ]
-        ]
-      ],
-      "footer" => [
-        "type" => "box",
-        "layout" => "horizontal",
-        "contents" => [
-          [
-            "type" => "text",
-            "text" => "View Details",
-            "size" => "lg",
-            "align" => "start",
-            "color" => "#0084B6",
-            "action" => [
-              "type" => "uri",
-              "label" => "View Details",
-              "uri" => "https://google.co.th/"
-            ]
-          ]
-        ]
-      ]
-    ]
-  
-  ];
+					"type" => "flex",
+					  "altText" => "Flex Message",
+					  "contents" => [
+						"type" => "bubble",
+						"header" => [
+						  "type" => "box",
+						  "layout" => "horizontal",
+						  "contents" => [
+							[
+							  "type" => "text",
+							  "text" => "งานประมูลที่กำลังจะถึง",
+							  "size" => "sm",
+							  "weight" => "bold",
+							  "color" => "#AAAAAA"
+							]
+						  ]
+						],
+						"body" => [
+						  "type" => "box",
+						  "layout" => "horizontal",
+						  "spacing" => "md",
+						  "contents" => [
+							[
+							  "type" => "image",
+							  "url" => "https://developers.line.me/assets/images/services/bot-designer-icon.png"
+							]
+						  ]
+						]
+					  ];
 			
 				$data = [
 				'replyToken' => $replyToken,
