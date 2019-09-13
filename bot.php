a8ad07cf7d874c40c27396cafa19ecd27aef09e5
watcharajeewi <35356635+watcharajeewi@users.noreply.github.com>
2019-09-13 17:58:19 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -25,17 +25,7 @@ if (!is_null($events['events'])) {
     "altText" => "งานประมูลที่กำลังจะถึง",
     "contents" => [
       "type" => "bubble",
-	"hero"=>[
-    "type"=> "image",
-    "url"=>"https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png",
-    "size"=> "full",
-    "aspectRatio"=> "20:13",
-    "aspectMode": "cover",
-    "action"=> [
-      "type"=> "uri",
-      "uri"=> "http://linecorp.com/"
-    ]
-  ],
+	
       "direction" => "ltr",
       "header" => [
         "type" => "box",
